bded973e64aad101f281dc9a26367584785275be
Howard Riddiough <howardriddiough@howworkmac.home>
2018-01-10 21:19:05 +0100

    adding auto git push

--- msw/spotCheck.py
+++ msw/spotCheck.py
@@ -1,6 +1,7 @@
 
 import msw
 import geoVisual as gv
+from autogit import autoGit
 
 print('Live MSW Forecast')
 
@@ -20,3 +21,8 @@ else:
     df = gv.mergeGeoData(forecast_df = df)
     # Draw and open geographic visualisation
     gv.drawSurfMap(df)
+    
+# Commit and push ressults to remote directory
+autoGit(local_directory='../', 
+        commit_comment='updating index.html',
+        author='HowardRiddiough')
