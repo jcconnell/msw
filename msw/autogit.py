9323125ee67b7b3f01198a4319dc9a58b6a65596
Howard Riddiough <howardriddiough@howworkmac.local>
2018-01-11 08:45:43 +0100

    autogit.py now working

--- msw/autogit.py
+++ msw/autogit.py
@@ -1,15 +1,22 @@
-from git import Repo,remote
+#from git import Repo,remote
 import os
+import git
 
 
-def autoGit(local_directory :str, commit_comment : str, author : str):
+def autoGit(local_directory : str,
+            file_to_add : str,
+            comment : str, 
+            author : str):
     """
     """
     # Intialise repository using local directory
-    repo = Repo(os.path.abspath(local_directory))
+    repo = git.Repo(os.path.abspath(local_directory))
+    # add file
+    repo.git.add(file_to_add)
     #  Git push
-    repo.git.commit('-m', commit_comment, author=author)
+    repo.git.commit('-m', comment, author=author)
     # Define origi
     origin = repo.remote(name='origin')
     # Push to remote directory
     origin.push()
+
